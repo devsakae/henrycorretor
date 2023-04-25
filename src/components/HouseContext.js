31156d397de948c21da381092338cc7f727bd8f2
Rodrigo Sakae <devsakae@gmail.com>
2023-04-25 13:57:06 -0300

    contexto instalado

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from '../data';
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState('Localização (todos)');
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState('Tipo (todos)');
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState('Faixa de preço (todos)');
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <HouseContext.Provider value={{
+      houses,
+      setHouses,
+      country,
+      setCountry,
+      countries,
+      setCountries,
+      property,
+      setProperty,
+      properties,
+      setProperties,
+      price,
+      setPrice,
+      loading,
+      setLoading,
+    }}>
+      { children }
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
 import { BrowserRouter as Router } from 'react-router-dom';
+import HouseContextProvider from './components/HouseContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
+  <HouseContextProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </HouseContextProvider>
 );
