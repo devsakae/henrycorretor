3f581843210e55cf85c7e8d957248e9583d8c6b8
Rodrigo Sakae <devsakae@gmail.com>
2023-04-28 16:36:10 -0300

    feat: transition with css

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import Search from './Search';
 
 const Banner = () => {
   return (
-    <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
+    <section className='h-full max-h-[640px] mb-8 xl:mb-24 banner'>
       <div className='flex flex-col lg:flex-row'>
         <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
           <h1 className='text-4x1 lg:text-[58px] font-semibold leading-none mb-6'>
