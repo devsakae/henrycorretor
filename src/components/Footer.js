2edfc1f4977566d827e114252d5eb3266cead950
Rodrigo Sakae <devsakae@gmail.com>
2023-04-29 01:14:04 -0300

    new build and footer credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <footer className='bg-black py-8 text-center text-white'>
       <div className='container mx-auto'>
-        Copyright 2023 Henry Simon Corretor Imobiliário
+        <p>
+          Copyright 2023 Henry Simon Corretor Imobiliário
+        </p>
+        <p>
+          Website desenvolvido por <a href="http://portfolio.sakae.social" className='font-semibold' target='blank'>Rodrigo Sakae</a>.
+        </p>
       </div>
     </footer>
   );
