14617a67aff8e56de6270d224dd863c4b4f84ac5
Rodrigo Sakae <devsakae@gmail.com>
2023-06-02 11:07:55 -0300

    feat: google-analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PropertyDetails from './pages/PropertyDetails';
 import Admin from './pages/Admin';
+import ReactGA from 'react-ga';
+ReactGA.initialize(process.env.REACT_APP_FIREBASE_MEASUREMENTID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   return (
