0ba68501f3a2d1e5679130649372a98ec04c2783
Rodrigo Sakae <devsakae@gmail.com>
2023-06-11 09:04:10 -0300

    fix: removed react-ga and installed analytics manually

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PropertyDetails from './pages/PropertyDetails';
 import Admin from './pages/Admin';
-import ReactGA from 'react-ga';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const App = () => {
   useEffect(() => {
-    ReactGA.initialize(process.env.REACT_APP_FIREBASE_MEASUREMENTID);
-    ReactGA.pageview(window.location.pathname + window.location.search);
     AOS.init();
   }, []);
 
--- src/index.js
+++ src/index.js
@@ -8,13 +8,28 @@ import PropertyContextProvider from './lib/PropertiesContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthContextProvider>
-    <PropertyContextProvider>
-      <Router>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </Router>
-    </PropertyContextProvider>
-  </AuthContextProvider>
+  <html>
+    <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PD22S7KPM1"></script>
+    <script>
+      {
+        `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-PD22S7KPM1');`
+      }
+    </script>
+    </head>
+    <body>
+      <AuthContextProvider>
+        <PropertyContextProvider>
+          <Router>
+            <React.StrictMode>
+              <App />
+            </React.StrictMode>
+          </Router>
+        </PropertyContextProvider>
+      </AuthContextProvider>
+    </body>
+  </html>
 );
